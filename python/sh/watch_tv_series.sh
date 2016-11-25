91473b51794d7a107eedb479ad1e948780854423
QuanLi <liquancsp@gmail.com>
2016-11-25 17:56:10 +0800

    add run shell script for watchTVSeries
    Signed-off-by: QuanLi <liquancsp@gmail.com>

--- python/sh/watch_tv_series.sh
+++ python/sh/watch_tv_series.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-cd ../src/watch_tv_series & pwd & nohup python ./watch_tvseries_crawler.py -c ../../conf/watch_tv_series.conf &
+cd ../
+if [ ! -d "log" ]; then
+   mkdir log
+fi
+if [ ! -d "data" ]; then
+   mkdir data
+fi
+cd ./src/watch_tv_series
+pwd
+nohup python ./watch_tvseries_crawler.py -c ../../conf/watch_tv_series.conf &
